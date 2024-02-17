f417d6d06a7d3a54203482e746d08ed8682cda2a
davidhernandez20 <luisdavidhernandez57@gmail.com>
2024-02-17 15:51:50 -0500

    se inicio modulo java con springboot y jdbc

---
+++ src/main/java/com/mycompany/modulo_java/Modulo_java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.modulo_java;
+
+/**
+ *
+ * @author wowda
+ */
+public class Modulo_java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
